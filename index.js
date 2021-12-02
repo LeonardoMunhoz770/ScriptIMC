456a88822606cd1c48fb84dd186321c8c85713ee
LeonardoMunhoz770 <munhozleonardo756@gmail.com>
2021-12-02 14:49:26 -0300

    Função somente numero e alterações no css

--- index.js
+++ index.js
@@ -14,12 +14,10 @@ function mostrarDados(){
   }else if(document.querySelector("#campo_peso").value == ''){
     alert("Campo [Peso] não foi preenchido")
   }else{
-    document.querySelector("#campo_resultado").innerHTML = `SEU IMC: ${calcularIMC().toFixed(2)}`
+    document.querySelector("#campo_resultado").innerHTML = `SEU IMC: <br>${calcularIMC().toFixed(2)}`
     verificarTabela();
   }
 }
-
-
 function verificarTabela(){
   if(calcularIMC().toFixed(2) <18.5){
     limparTabela();
@@ -38,14 +36,12 @@ function verificarTabela(){
     $("#maior_40").addClass("tabela_selecao");
   }
 }
-
 const limparDados = () =>{
   document.querySelector("#campo_altura").value = '';
   document.querySelector("#campo_peso").value = '';
   document.querySelector("#campo_resultado").innerHTML = '';
   limparTabela();
 }
-
 const limparTabela = () =>{
   $("#menor_18").removeClass('tabela_selecao');
   $("#menor_25").removeClass('tabela_selecao');
@@ -53,10 +49,22 @@ const limparTabela = () =>{
   $("#menor_40").removeClass('tabela_selecao');
   $("#maior_40").removeClass('tabela_selecao');
 }
-
 document.querySelector("#calcular").addEventListener("click", mostrarDados);
 document.querySelector("#limpar").addEventListener("click", limparDados);
-
 limparDados();
+
+
+
+function onlynumber(evt) {
+  var theEvent = evt || window.event;
+  var key = theEvent.keyCode || theEvent.which;
+  key = String.fromCharCode( key );
+  //var regex = /^[0-9.,]+$/;
+  var regex = /^[0-9.]+$/;
+  if( !regex.test(key) ) {
+     theEvent.returnValue = false;
+     if(theEvent.preventDefault) theEvent.preventDefault();
+  }
+}
 
 
